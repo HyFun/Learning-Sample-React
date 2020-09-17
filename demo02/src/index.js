437ef79604e2a75aa3f716e07facdbcb07f0e67e
heyongfeng <heyongfeng@cloudwalk.com>
2020-09-17 15:38:39 +0800

    更新一下

--- demo02/src/index.js
+++ demo02/src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 // import * as serviceWorker from './serviceWorker';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root')
 
 /**
  * 1. 元素和组件
@@ -16,11 +16,11 @@ const root = document.getElementById("root");
 /**
  * 2. 实现页面中实时时间的展示
  */
-function Clock() {
-  ReactDOM.render(<h1>现在时间：{new Date().toLocaleString()}</h1>, root);
+function Clock () {
+  ReactDOM.render(<h1>现在时间：{new Date().toLocaleString()}</h1>, root)
 }
 Clock()
-setInterval(Clock, 1000);
+setInterval(Clock, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
