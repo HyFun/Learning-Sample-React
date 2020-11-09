cecdf720e242ecec47bc8ab52ab6d82876fff27a
hyfun <775183940@qq.com>
2020-11-09 19:46:23 +0800

    update

--- demo05/src/views/login/index.js
+++ demo05/src/views/login/index.js
@@ -1,19 +1,58 @@
-import './index.scss';
+import "./index.scss";
 
 // react
-import React from 'react'
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-class Login extends React.Component {
-    render() {
-        return (
-            <div className="form_wrap">
-                <div className="form_header">
-                    <h2>登录</h2>
-                    <span>账号注册</span>
-                </div>
-            </div>
-        )
-    }
-}
+const Login = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div className="form_wrap">
+      <div className="form_header">
+        <h2>登录</h2>
+        <span>账号注册</span>
+      </div>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!",trigger: 'blur' }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
 
-export default Login
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
